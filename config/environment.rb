34c82f95e1ad1365459ea912327447bd4214e370
Mike <mclancy96@gmail.com>
2025-07-22 15:48:56 -0400

    Initialize and create

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ Bundler.require
 
 require_relative '../lib/dog'
 
-DB = { conn: SQLite3::Database.new("db/dogs.db") }
+DB = { conn: SQLite3::Database.new('db/dogs.db') }
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+  attr_accessor :id, :name, :breed
 
+  def initialize(name:, breed:, id: nil)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
 end
