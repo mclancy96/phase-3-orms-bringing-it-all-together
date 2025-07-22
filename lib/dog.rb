303d53a500d1944d5a61bf0abe4cf0c6c7a860f6
Mike <mclancy96@gmail.com>
2025-07-22 15:54:28 -0400

    Drop table and save

--- lib/dog.rb
+++ lib/dog.rb
@@ -17,4 +17,20 @@ class Dog
     SQL
     DB[:conn].execute(sql)
   end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS dogs;
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed) VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, @name, @breed)
+    @id = DB[:conn].last_insert_row_id
+    self
+  end
 end
